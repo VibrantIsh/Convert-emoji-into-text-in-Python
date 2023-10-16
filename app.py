1056520d6841238f22a33db23264cc88ef3a63b4
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-10-16 23:23:19 +0530

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
+# I don't have any plans for today, feeling quite exhausted. Just making a random effort to maintain my streak.
+
+import emoji
+text_with_emoji = "I love Python! 😍🐍"
+text_with_emoji_converted = emoji.demojize(text_with_emoji)
+print(text_with_emoji_converted)
 
